bb4b88d14050a6d83d12bda8fafdb417339b1f84
Fabien CAYRE <fabiencayre81@gmail.com>
2022-10-29 23:33:18 +0300

    Increase fibonnaci

--- Fibonnaci.java
+++ Fibonnaci.java
@@ -12,7 +12,7 @@ public class Fibonnaci {
   }
 
   public static void main(String[] args) {
-    int count = 10;
+    int count = 30;
     System.out.print(n1 + " " + n2); // printing 0 and 1
     printFibonacci(count - 2); // n-2 because 2 numbers are already printed
   }
